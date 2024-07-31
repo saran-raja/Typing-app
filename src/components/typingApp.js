4aca402b52075e7ff3d941dc540510d00c4da986
saran2653 <saran26053@gmail.com>
2024-07-31 20:40:39 +0530

    first commit

---
+++ src/components/typingApp.js
@@ -0,0 +1,157 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./home.css";
+const TypingApp = () => {
+  const [typedWord, setTypedWord] = useState("");
+  const [splittedWords, setSplittedWords] = useState([]);
+  const [words, setWords] = useState([]);
+  const [alphabet, setAlphabet] = useState([]);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
+  const [wordsTyped, setWordsTyped] = useState(0);
+  const randomNumber = Math.floor(Math.random() * (20 - 12) + 12);
+  console.log(randomNumber);
+  const randomLength = Math.floor(Math.random() * (7 - 4) + 4);
+  const url = `https://random-word-api.herokuapp.com/word?length=${randomLength}&number=${randomNumber}`;
+
+  const fetchWords = () => {
+    axios
+      .get(url)
+      .then((response) => {
+        const fetchedWords = response.data.map((word) => word + " ");
+        setWords(fetchedWords);
+        const newSplittedWords = fetchedWords.map((word) =>
+          word
+            .split("")
+            .map((letter) => ({ letter, matched: false, incorrect: false }))
+        );
+        setSplittedWords(newSplittedWords);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchWords();
+
+    const alphabetArray = [];
+    for (let i = 65; i <= 90; i++) {
+      alphabetArray.push(String.fromCharCode(i));
+    }
+    alphabetArray.push(" ");
+    setAlphabet(alphabetArray);
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (splittedWords.length > 0) {
+        const currentWord = splittedWords[currentWordIndex];
+        const currentLetter = currentWord[currentLetterIndex].letter;
+
+        if (event.key === currentLetter) {
+          const updatedCurrentWord = currentWord.map((letterObj, index) => ({
+            ...letterObj,
+            matched: index <= currentLetterIndex ? true : letterObj.matched,
+            incorrect:
+              index === currentLetterIndex ? false : letterObj.incorrect,
+          }));
+          // console.log(currentLetter);
+          const updatedSplittedWords = [...splittedWords];
+          updatedSplittedWords[currentWordIndex] = updatedCurrentWord;
+
+          setSplittedWords(updatedSplittedWords);
+          setCurrentLetterIndex((prevIndex) => prevIndex + 1);
+          setTypedWord((prevTypedWord) => prevTypedWord + event.key);
+          // console.log(updatedSplittedWords);
+          // setSplittedWords(updatedSplittedWords);
+          //           setCurrentLetterIndex((prevIndex) => prevIndex + 1);
+          // setTypedWord((prevTypedWord) => prevTypedWord + event.key);
+          if (currentLetterIndex + 1 === currentWord.length) {
+            setWordsTyped((prevWordsTyped) => prevWordsTyped + 1);
+            setCurrentWordIndex((prevIndex) => prevIndex + 1);
+            setCurrentLetterIndex(0);
+            setTypedWord("");
+
+            if (currentWordIndex + 1 === splittedWords.length) {
+              fetchWords();
+              setCurrentWordIndex(0);
+              setWordsTyped(0);
+            }
+          }
+        } else {
+          const updatedCurrentWord = currentWord.map((letterObj, index) => ({
+            ...letterObj,
+            incorrect:
+              index === currentLetterIndex ? true : letterObj.incorrect,
+          }));
+
+          const updatedSplittedWords = [...splittedWords];
+          updatedSplittedWords[currentWordIndex] = updatedCurrentWord;
+
+          setSplittedWords(updatedSplittedWords);
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [currentWordIndex, currentLetterIndex, splittedWords]);
+
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="home col-7">
+            <div className="col-12 paracontent d-flex flex-column justify-content-center">
+              <section className="col-12 para d-flex flex-row  p-4">
+                {splittedWords.map((wordArr, index) => (
+                  <p key={index}>
+                    {wordArr.map((letterObj, id) => (
+                      <span
+                        key={id}
+                        className={
+                          index === currentWordIndex &&
+                          id === currentLetterIndex
+                            ? "current-letter"
+                            : ""
+                        }
+                        style={{
+                          color: letterObj.matched
+                            ? "green"
+                            : letterObj.incorrect
+                            ? "red"
+                            : "black",
+                          textDecoration:
+                            index === currentWordIndex &&
+                            id === currentLetterIndex
+                              ? "underline"
+                              : "none",
+                        }}
+                      >
+                        {letterObj.letter}
+                      </span>
+                    ))}{" "}
+                    {" _"}
+                  </p>
+                ))}
+              </section>
+            </div>
+            <section className="keys d-flex justify-content-center col-12 pt-5">
+              {alphabet.map((item, index) => (
+                <div key={index} className="col-2 key-item">
+                  {item}
+                </div>
+              ))}
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TypingApp;
